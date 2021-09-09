aefc168ba7f6d0771bab838569821d366a902485
Gabe Larson <gabriel.g.larson@gmail.com>
2021-09-09 11:25:09 -0700

    Update Inheritance.py

--- Inheritance.py
+++ Inheritance.py
@@ -1,14 +1,14 @@
-class car:
+class car: #created a class named car and gave it attributes
     num_wheels = 4
     speed = 70
     material = "metal"
 
-class mustang(car):
+class mustang(car): #child car named mustang has its own attributes along with the 'car' attributes
     color = "red"
     doors = 2
     
 
-class civic(car):
+class civic(car): #child car named civic has its own attributes along with the 'car' attributes
     year = 2009
     transmission = "manual"
     
