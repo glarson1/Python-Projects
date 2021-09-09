5d9138419544a7caa8886acea598ef4973f95a14
Gabe Larson <gabriel.g.larson@gmail.com>
2021-09-09 11:23:09 -0700

    Create Inheritance.py

---
+++ Inheritance.py
@@ -0,0 +1,14 @@
+class car:
+    num_wheels = 4
+    speed = 70
+    material = "metal"
+
+class mustang(car):
+    color = "red"
+    doors = 2
+    
+
+class civic(car):
+    year = 2009
+    transmission = "manual"
+    
